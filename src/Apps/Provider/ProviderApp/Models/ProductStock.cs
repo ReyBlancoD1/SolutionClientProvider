6a0c444c372b99ee573b7f9b2be416ac2e337491
Carlos Rodriguez <CRodriguez3@carnival.com>
2020-04-22 03:05:48 -0500

    App simple para consultar ProductoXStock y actualizar Stock.

---
+++ src/Apps/Provider/ProviderApp/Models/ProductStock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ProviderApp.Models
+{
+    public class ProductStock
+    {
+        public int productId { get; set; }
+
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+        public decimal price { get; set; }
+
+        public int? stock { get; set; }
+
+    }
+}
---
+++ src/Apps/Provider/ProviderApp/Models/Stock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ProviderApp.Models
+{
+    public class Stock
+    {       
+        public string hasItems { get; set; }
+
+        public List<ProductStock> items { get; set; }
+    }
+}
